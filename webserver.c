5cc853200ad021bb852093db91b7f0aad887cd45
Jericho0318 <111917174+Jericho0318@users.noreply.github.com>
2024-12-10 20:42:05 -0800

    Update webserver.c

--- webserver.c
+++ webserver.c
@@ -1,10 +1,11 @@
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
-#include <pthread.h>
-#include <stdbool.h>
+
 #include "request.h"
 
 #define DEFAULT_PORT 8080
@@ -13,8 +14,7 @@ int total_requests = 0;
 long total_bytes_received = 0;
 long total_bytes_sent = 0;
 
-
-int check(int exp, const char *msg) {
+int check(int exp, const char* msg) {
     if (exp < 0) {
         perror(msg);
         exit(1);
@@ -28,9 +28,8 @@ void* handle_connection(void* sock_fd_ptr) {
 
     while (1) {
         Request* req = request_read_from_fd(sock_fd);
-        if (req == NULL) {
+        if (req == NULL)
             break;
-        }
 
         total_requests++;
 
@@ -53,7 +52,8 @@ void* handle_connection(void* sock_fd_ptr) {
             char calc_response[1024];
             snprintf(calc_response, sizeof(calc_response),
                      "HTTP/1.1 200 OK\nContent-Type: text/plain\n\n"
-                     "Result: %d", a + b);
+                     "Result: %d",
+                     a + b);
             send(sock_fd, calc_response, strlen(calc_response), 0);
         } else {
             const char* not_found = "HTTP/1.1 404 Not Found\n\n";
@@ -67,33 +67,35 @@ void* handle_connection(void* sock_fd_ptr) {
     return NULL;
 }
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const* argv[]) {
     int port;
-    if (argc == 3 && strcmp(argv[1], "-p") == 0) {
+    if (argc == 3 && strcmp(argv[1], "-p") == 0)
         port = atoi(argv[2]);
-    } else if (argc == 2 && strcmp(argv[1], "-p") == 0) {
+    else if (argc == 2 && strcmp(argv[1], "-p") == 0)
         port = DEFAULT_PORT;
-    }
 
     int socket_fd, client_sock;
     struct sockaddr_in sock_addr, client_addr;
     socklen_t client_addr_size;
 
-    check(socket_fd = socket(AF_INET, SOCK_STREAM, 0), "Failed to create socket");
+    check(socket_fd = socket(AF_INET, SOCK_STREAM, 0),
+          "Failed to create socket");
 
     memset(&sock_addr, 0, sizeof(sock_addr));
     sock_addr.sin_family = AF_INET;
     sock_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     sock_addr.sin_port = htons(port);
 
-    check(bind(socket_fd, (struct sockaddr*)&sock_addr, sizeof(sock_addr)), "Failed to bind");
+    check(bind(socket_fd, (struct sockaddr*)&sock_addr, sizeof(sock_addr)),
+          "Failed to bind");
     check(listen(socket_fd, 5), "Failed to listen");
 
     printf("Server listening on port %d\n", port);
     while (1) {
         client_addr_size = sizeof(client_addr);
-        check(client_sock = accept(socket_fd, (struct sockaddr*)&client_addr, &client_addr_size), "Failed to accept");
-
+        check(client_sock = accept(socket_fd, (struct sockaddr*)&client_addr,
+                                   &client_addr_size),
+              "Failed to accept");
         int* client_sock_ptr = malloc(sizeof(int));
         *client_sock_ptr = client_sock;
         pthread_t thread;
